69755e819c7075efc68106a4ae3c6522c95af2c1
Alexandra <103836628+abaivel@users.noreply.github.com>
2022-05-21 12:02:23 +0200

    Add files via upload

---
+++ src/main/java/com/example/eco_basket/GameActivity.kt
@@ -0,0 +1,132 @@
+package com.example.eco_basket
+import android.annotation.SuppressLint
+import android.graphics.Rect
+import android.os.Bundle
+import android.os.Handler
+import android.util.DisplayMetrics
+import android.view.MotionEvent
+import android.view.View
+import android.view.Window
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class GameActivity : AppCompatActivity() {
+    private lateinit var objet : ImageView
+    private lateinit var poubelleVerte : ImageView
+    private lateinit var poubelleMarron : ImageView
+    private lateinit var poubelleGrise : ImageView
+    private lateinit var poubelleJaune : ImageView
+    private lateinit var faux : ImageView
+    private lateinit var vrai : ImageView
+    private lateinit var ville : String
+    private val infos = mutableMapOf<String,String>()
+    private var objets = listOf("Bouteille d'eau en plastique"," Canette de soda"," Brique de jus"," Carton"," Boite de conserve"," Thon en conserve"," Pot de confiture"," Feuille de papier"," Reste de pomme"," Epluchures"," Bouteille d'eau en verre"," Pot de yaourt en plastique"," Pot de yaourt en verre"," Barquette en plastique"," Flacon de shampoing"," Flacon de parfum")
+    private var d=0
+    private var yObjet=0
+    private var poubelles = mutableListOf<ImageView>()
+    private var nomObjetPhotos = mapOf("Bouteille d'eau en plastique" to R.drawable.bouteille, "Carton" to R.drawable.carton, "Reste de pomme" to R.drawable.pomme)
+    private var photosObjet = listOf(R.drawable.bouteille,R.drawable.carton,R.drawable.pomme)
+    private var objetsPoubelles =mapOf(R.drawable.bouteille to "poubelleJaune",R.drawable.carton to "poubelleJaune",R.drawable.pomme to "poubelleVerte")
+    private var nomObjet = R.drawable.bouteille
+    private var num=0
+    private val SHARED_CITY_INFO = "SHARED_CITY_INFO"
+    private val SHARED_CITY_NAME = "SHARED_CITY_NAME"
+    @SuppressLint("ClickableViewAccessibility", "UseCompatLoadingForDrawables")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+        supportActionBar?.hide()
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        recup_ville()
+        val width = displayMetrics.widthPixels
+        val height = displayMetrics.heightPixels
+        println("largeur=" + width + "  hauteur=" + height)
+        findViewById<ImageView>(R.id.objet).also { objet = it }
+        findViewById<ImageView>(R.id.poubelleVerte).also { poubelleVerte = it }
+        findViewById<ImageView>(R.id.compost).also { poubelleMarron = it }
+        findViewById<ImageView>(R.id.poubelleGrise).also { poubelleGrise = it }
+        findViewById<ImageView>(R.id.poubelleJaune).also { poubelleJaune = it }
+        findViewById<ImageView>(R.id.faux).also { faux = it }
+        findViewById<ImageView>(R.id.vrai).also { vrai = it }
+        poubelles = mutableListOf(poubelleMarron, poubelleJaune, poubelleGrise, poubelleVerte)
+        objet.post {
+            d = (height - (objet.y + objet.height)).toInt()
+            yObjet = objet.y.toInt()
+            println("d = $d")
+        }
+        objet.setOnLongClickListener {
+            println("appui")
+            true
+        }
+        objet.setOnTouchListener { v, event ->
+
+            //@SuppressLint("ClickableViewAccessibility")
+            if (event?.action == MotionEvent.ACTION_UP) {
+                for (p in poubelles) {
+                    if (((p.x) <= objet.x && objet.x <= (p.x + p.width - objet.width)) && ((p.y) <= objet.y && objet.y <= (p.y + p.height - objet.height))) {
+                        println(p.tag)
+                        println(objetsPoubelles[nomObjet])
+                        if (p.tag == objetsPoubelles[nomObjet]) {
+                            vrai.visibility = View.VISIBLE
+                            Handler().postDelayed({ vrai.visibility = View.INVISIBLE }, 1000)
+                            num += 1
+                            if (num < photosObjet.size) {
+                                nomObjet = photosObjet[num]
+                                objet.background = resources.getDrawable(nomObjet)
+                                objet.x = ((width - objet.width) / 2).toFloat()
+                                //objet.y=(height-objet.y-d).toFloat()
+                                objet.y = (yObjet).toFloat()
+                            } else {
+                                objet.visibility = View.INVISIBLE
+                            }
+                        } else {
+                            faux.visibility = View.VISIBLE
+                            Handler().postDelayed({ faux.visibility = View.INVISIBLE }, 1000)
+                            objet.x = ((width - objet.width) / 2).toFloat()
+                            //objet.y= (height-objet.y-d)
+                            objet.y = (yObjet).toFloat()
+                        }
+                    }
+                }
+            } else {
+                val x = event?.rawX?.toInt()
+                val y = event?.rawY?.toInt()
+                if (y != null) {
+                    objet.y = y.toFloat() - objet.height / 2 - getStatusBarHeight()
+                }
+                if (x != null) {
+                    objet.x = x.toFloat() - objet.width / 2
+                }
+                val nx=objet.x
+                val ny =objet.y
+                println("$nx   $ny")
+                println("$x $y")
+            }
+            v?.onTouchEvent(event) ?: true
+        }
+    }
+    private fun getStatusBarHeight(): Int{
+        val rectangle = Rect()
+        val window = window
+        window.decorView.getWindowVisibleDisplayFrame(rectangle)
+        val statusBarHeight: Int = rectangle.top
+        val contentViewTop = window.findViewById<View>(Window.ID_ANDROID_CONTENT).top
+        val titleBarHeight = contentViewTop - statusBarHeight
+        println("taille barre $statusBarHeight")
+        return statusBarHeight
+    }
+
+    private fun recup_ville(){
+        ville = getSharedPreferences(SHARED_CITY_INFO, MODE_PRIVATE).getString(
+            SHARED_CITY_NAME,
+            null)!!
+        for (o in objets){
+            val m = getSharedPreferences(SHARED_CITY_INFO, MODE_PRIVATE).getString(
+                o,
+                null)!!
+            infos[o] = m
+        }
+
+    }
+}
---
+++ src/main/java/com/example/eco_basket/MainActivity.kt
@@ -0,0 +1,139 @@
+package com.example.eco_basket
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.nio.charset.Charset
+
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("ClickableViewAccessibility")
+    private lateinit var bouton : Button
+    private lateinit var ecran : ConstraintLayout
+    private lateinit var info_button : ImageButton
+    private lateinit var infos : TextView
+    private lateinit var ville : String
+    private lateinit var infos_ville : List<String>
+    private lateinit var objets : List<String>
+    private val SHARED_CITY_INFO = "SHARED_CITY_INFO"
+    private val SHARED_CITY_NAME = "SHARED_CITY_NAME"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        bouton=findViewById(R.id.bouton_depart)
+        bouton.setOnClickListener {
+            enregistrer_ville(ville)
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+        }
+        println(recup_data("Paris (75)").size)
+        ecran = findViewById(R.id.ecran)
+        val search = findViewById<SearchView>(R.id.search)
+        val listView = findViewById<ListView>(R.id.listview)
+        val cities = resources.getStringArray(R.array.cities)
+        info_button = findViewById(R.id.button_info)
+        infos = findViewById(R.id.info_villes)
+        val adapter: ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_1,resources.getStringArray(R.array.cities))
+        listView.adapter = adapter
+        listView.onItemClickListener = AdapterView.OnItemClickListener{ _, view, position, _ ->
+            ville = adapter.getItem(position).toString()
+            search.setQuery(ville, false)
+            listView.visibility = View.INVISIBLE
+            view.hideKeyboard()
+            bouton.isEnabled=true
+
+        }
+        search.setOnClickListener { v ->
+            when (v.id) {
+                R.id.search -> search.onActionViewExpanded()
+            }
+        }
+        info_button.setOnClickListener {
+            if (infos.visibility == View.VISIBLE) {
+                infos.visibility = View.INVISIBLE
+            } else {
+                infos.visibility = View.VISIBLE
+            }
+        }
+        search.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                search.clearFocus()
+                if (cities.contains(p0)){
+                    adapter.filter.filter(p0)
+                }else{
+                    Toast.makeText(applicationContext, "Item not found", Toast.LENGTH_LONG).show()
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                adapter.filter.filter(p0)
+                listView.visibility = View.VISIBLE
+                bouton.isEnabled=false
+                return false
+            }
+
+        })
+
+    }
+    private fun View.hideKeyboard() {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(windowToken, 0)
+    }
+    fun View.showKeyboard() {
+        requestFocus()
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+    }
+
+    fun enregistrer_ville(ville: String){
+        getSharedPreferences(SHARED_CITY_INFO, MODE_PRIVATE)
+            .edit()
+            .putString(SHARED_CITY_NAME, ville)
+            .apply()
+        infos_ville = recup_data(ville)
+        if (infos_ville.size >0) {
+            for (i in 0 until infos_ville.size) {
+                getSharedPreferences(SHARED_CITY_INFO, MODE_PRIVATE)
+                    .edit()
+                    .putString(objets[i], infos_ville[i])
+                    .apply()
+            }
+        }else{
+            println("ERREUR POUR RECUPERER LES DONNEES")
+        }
+    }
+
+    private fun recup_data(ville: String): List<String> {
+        val inputStream: InputStream = resources.openRawResource(R.raw.donnees)
+        val reader = BufferedReader(InputStreamReader(inputStream, Charset.forName("UTF-8")))
+        reader.readLines().forEach {
+
+            //get a string array of all items in this line
+            val items = it.split(",")
+            println("taille="+items.size)
+            if (items[0]=="Villes"){
+                objets=items.subList(1,items.size)
+            }
+            if (items[0]==ville){
+                return items.subList(1,items.size)
+            }
+            //do what you want with each item
+        }
+        return emptyList()
+    }
+
+}
+
+
